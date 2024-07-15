17ec48d475c7aaa3fdc0d945f1e1a053ff45532a
Stanley J <jeongstanley@gmail.com>
2024-07-14 20:40:24 -0700

    server render is working

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import Button from 'react-bootstrap/Button';
 import {NavLink} from 'react-router-dom';
 import "./HomePage.css";
-import goodreadsIcon from '../../imgs/goodreads_logo_banner.png'
+import GoodreadsIcon from './goodreads_logo_banner.png'
 
 export default function HomePage() {
 
@@ -11,14 +11,13 @@ export default function HomePage() {
       <div className="row">
         <div className="col col-7 my-5">
             <div className="container">
-              <img className="bannerlogo" src={goodreadsIcon} width="80%" alt="GoodReads logo banner" />
+              <img className="bannerlogo" src={GoodreadsIcon} width="80%" alt="GoodReads logo banner" />
               
               <h4 id="title" className="display-4">Book Reviews</h4>
               
               <hr className="my-3"></hr>
               <p className="lead">Search for your favorite books by title, author, or ISBN.</p>
               <p>Log in to review and leave ratings!</p>
-              <p className="lead"></p>
                 <NavLink className="animateMe btn btn-primary btn-lg" to="/login" role="button">Login</NavLink>
             </div>
         </div>
